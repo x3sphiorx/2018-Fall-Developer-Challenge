3092f6584aa705de4a640737ad6c67073dbd67d8
x3xphi0r <snowy_moments@live.com>
2018-09-05 01:57:53 +0800

    Basicblock Chain
    Completion of the required basic blockchain task.

--- Part2-SimplestBlockchainEver/simple_coin.py
+++ Part2-SimplestBlockchainEver/simple_coin.py
@@ -3,17 +3,30 @@ from blockchain import Blockchain
 #Initiates blockchain
 
 def generate_blocks(number_of_blocks):
-    """
-    input: number_of_blocks: int
-    output: hash of final block: str
+    # """
+    # input: number_of_blocks: int
+    # output: hash of final block: str
+    #
+    # Given a number of blocks return the output after x amount of block as a string. Input blocks are EMPTY
+    # """
 
-    Given a number of blocks return the output after x amount of block as a string. Input blocks are EMPTY
-    """
-    
+    # instantiate the blockchain class to create the gensis block
+    bc_data = Blockchain()
 
-    ### YOUR CODE HERE
+    # Since the test case would be testing on the number of blocks in the chain
+    # a loop is needed such that it would iterate until it match the number of
+    # counts of block required, which is 5 and 10
 
-   
+    # Terminating condition is set to 1. Reverse iteration
+    while number_of_blocks != 1:
+        # Calling the mine method would automatically generate new block with the parent block's hash been supply in.
+        # To get the parent block is to call the "get_current_block" method which return the latest block in the chain
+        bc_data.mine(bc_data.get_current_block())
 
+        # After hashing of the new block with the previous block hash, decrease count
+        number_of_blocks -= 1
 
-    
+    # Return the hash of the latest block by calling the same method again "get_current_block", follow by access the
+    # block attributes, "hash" in this case
+    return bc_data.get_current_block().hash
+
--- Part2-SimplestBlockchainEver/test_simple_coin.py
+++ Part2-SimplestBlockchainEver/test_simple_coin.py
@@ -4,7 +4,7 @@ from simple_coin import *
 class TestMerkleProof(unittest.TestCase):
 
     def test_five_blocks(self):
-        """Test that the blockchain is able to generate 5 blcoks with the 5th block containing the correct hash
+        """Test that the blockchain is able to generate 5 blocks with the 5th block containing the correct hash
         """
         self.assertEqual('9400b8b82ca50e77cb017aa910f5c7a31541b2c0a95a00fba41dec6da7e18da7', generate_blocks(5))
 
@@ -12,4 +12,3 @@ class TestMerkleProof(unittest.TestCase):
         """Same Test but with 10 blocks
         """
         self.assertEqual('ac328102ec4df7f81e5c6f9701ccd7d80d21c94fe716d40061562f1ebc967568', generate_blocks(10))
-        
